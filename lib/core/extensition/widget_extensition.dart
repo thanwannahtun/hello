fe4f4a0d1c11d7495d4ccf6ff4a243c8af4ce7ca
thanwanna <my.thanwanna@gmail.com>
2024-05-20 23:25:57 +0630

    extensition - padding

---
+++ lib/core/extensition/widget_extensition.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+extension ColumnExtension on Column {
+  Padding padding({required EdgeInsetsGeometry padding}) {
+    return Padding(padding: padding);
+  }
+}
